123afb903da3d07451d1fee15b22e28a7451d046
J.A. Aartsen <jamal.aartsen@wearetriple.com>
2022-08-29 12:10:00 +0200

    Made api ready to publish on Heroku

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.post("/createCharacter", (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log("Herofy is running on port 3000..")
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Herofy is running on port ${PORT}...`)
 })
