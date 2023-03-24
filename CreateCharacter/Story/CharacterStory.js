4beaf85e98386919d1eacd862e06eddd4ad859e1
J.A. Aartsen <jamal.aartsen@wearetriple.com>
2023-03-24 19:40:25 +0100

    Add comment to generateStory method

--- CreateCharacter/Story/CharacterStory.js
+++ CreateCharacter/Story/CharacterStory.js
@@ -1,5 +1,6 @@
 import story from "./ChatGPT.js"
 
+// TODO: Add some more information like: Home world etc.
 export default async function generateStory(name, isHero, alias, typeBeing) {
     return await story(name, isHero, alias, typeBeing)
 }
