e32220eb3e0ade02dc470e38fcff7f1973bd0a32
J.A. Aartsen <jamal.aartsen@wearetriple.com>
2023-03-24 19:35:09 +0100

    Change parameter 'being' from CreateCharacter method to 'typeBeing'

--- CreateCharacter/CreateCharacter.js
+++ CreateCharacter/CreateCharacter.js
@@ -3,9 +3,9 @@ import generateStory from "./Story/CharacterStory.js"
 import Character from "./Character.js"
 import type from "./TypeOfCharacter.js"
 
-export default async function createCharacter(name, being, alias, avgPowerLevel, isHero) {
+export default async function createCharacter(name, typeBeing, alias, avgPowerLevel, isHero) {
     let generatedPowers = generatePowers(avgPowerLevel)
-    let story = await generateStory(name, isHero, alias, being)
+    let story = await generateStory(name, isHero, alias, typeBeing)
 
-    return new Character(name, being, type(isHero), alias, avgPowerLevel, generatedPowers, story)
+    return new Character(name, typeBeing, type(isHero), alias, avgPowerLevel, generatedPowers, story)
 }
